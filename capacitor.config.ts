9aa90f0ef8827f7271949459912d49fe06d5dd67
ViktorTrojan <87524553+ViktorTrojan@users.noreply.github.com>
2023-03-21 12:51:20 +0100

    first init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'htl-inside',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
