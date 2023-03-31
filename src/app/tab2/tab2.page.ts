ad515b0920e2b079a08b3f39033f4b8b2e0d0afe
UnknownFloo <96895568+UnknownFloo@users.noreply.github.com>
2023-03-31 10:56:23 +0200

    Neue API Adresse

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import axios, { AxiosRequestConfig } from "axios";
 import { NavController, IonTabs } from '@ionic/angular';
+import { getAllSchueler, getSchueler } from 'src/axios-instance';
 
 interface Student {
   vname: string;
@@ -22,19 +23,31 @@ export class Tab2Page implements OnInit {
     this.getAllSchueler().then((data: Student[]) => {
       this.students = data;
     });
+    
   }
 
-  constructor(private navCtrl: NavController, private tabs: IonTabs) {}
-  
+  constructor(private navCtrl: NavController, private tabs: IonTabs) {
+
+  }
+  async fetchAllSchueler() {
+    const users = await getAllSchueler();
+    console.log(users);
+    
+  }
+
+  async fetchSchueler(id: number) {
+    const user = await getSchueler(id);
+    console.log(user);
+  }
+
   async getAllSchueler(){
     try{
-      const response = await axios.get("https://hies.lueger.cloud/getMainPage");
+      const response = await axios.get("https://api.susnext.com/getMainPage");
       console.log(response.data)
       return response.data;
     }catch (error){
       console.error(error)
     }
-    
   }
 
   goToTab3() {
