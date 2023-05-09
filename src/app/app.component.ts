b50cb0694b8b3515c529de3599a00b31c0f0bcd5
UnknownFloo <96895568+UnknownFloo@users.noreply.github.com>
2023-05-09 08:40:11 +0200

    Statusbar Funktion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,19 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit{
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2) {
+    const hideStatusBar = async () => {
+      await StatusBar.setOverlaysWebView({ overlay: true });
+    };
+
+    hideStatusBar();
+  }
+  
 
   ngOnInit() {
     const selectedTheme = localStorage.getItem("selectedTheme") || 'dark'; // set the default theme to 'dark' or load from localstorage
     this.renderer.setAttribute(document.body, 'color-theme', selectedTheme);
   }
+
+ 
 }
